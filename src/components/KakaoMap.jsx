e6bad18bcf1822526721f351ded64bd1e5c55092
Yoos99 <yoosh2520@naver.com>
2024-10-04 23:28:30 +0900

    feat: 카드 닫기

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -3,15 +3,14 @@ import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import AreaCard from './AreaCard';
 
 function KakaoMap({ areaData }) {
-  const [selectedArea, setSelectedArea] = useState(null); // 선택된 지역 정보 저장
+  const [selectedArea, setSelectedArea] = useState(null);
 
   return (
-    <div style={{ position: 'relative' }}>
+    <div>
       <Map
         center={{ lat: 37.55659428234287, lng: 126.97302795181167 }}
         style={{ width: '1500px', height: '1000px' }}
         level={7}
-        onClick={() => setSelectedArea(null)} // 맵을 클릭하면 카드 닫기
       >
         {/* 모든 지역 데이터를 이용해 마커를 생성 */}
         {areaData &&
@@ -21,17 +20,14 @@ function KakaoMap({ areaData }) {
               key={index}
               position={{ lat: parseFloat(area.x), lng: parseFloat(area.y) }}
               clickable={true}
-              onClick={() => setSelectedArea(area)} // 마커 클릭 시 지역 정보 설정
+              onClick={() => setSelectedArea(area)}
             />
           ))}
       </Map>
 
-      {/* 선택된 지역의 상세 정보를 출력 (카드) */}
+      {/* 선택된 지역의 상세 정보를 출력 */}
       {selectedArea && (
-        <AreaCard
-          area={selectedArea}
-          onClose={() => setSelectedArea(null)} // 카드 닫기 버튼 설정
-        />
+        <AreaCard area={selectedArea} onClose={() => setSelectedArea(null)} />
       )}
     </div>
   );
