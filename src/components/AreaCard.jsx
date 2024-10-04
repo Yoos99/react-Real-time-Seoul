9bb4e886adf2cb5e3ce16a04868417b5c1018381
Yoos99 <yoosh2520@naver.com>
2024-10-04 23:27:46 +0900

    feat: 카드 내용 순서 변경

--- src/components/AreaCard.jsx
+++ src/components/AreaCard.jsx
@@ -3,14 +3,6 @@ import styles from '../css/AreaCard.module.scss';
 const AreaCard = ({ area, onClose }) => {
   return (
     <div className={styles.card}>
-      <div className={styles.header}>
-        <span className={styles.name}>{area.area_nm}</span>
-        {/* 닫기 버튼 추가 */}
-        <button className={styles.closeButton} onClick={onClose}>
-          ✖
-        </button>
-      </div>
-
       <div className={styles.imageWrapper}>
         <img
           src={`https://data.seoul.go.kr/SeoulRtd/images/hotspot/${area.area_nm}.jpg`}
@@ -18,7 +10,14 @@ const AreaCard = ({ area, onClose }) => {
           className={styles.image}
         />
       </div>
-      <div className={styles.triangle}></div>
+      <div className={styles.content}>
+        <span className={styles.name}>{area.area_nm}</span>
+        {/* 닫기 버튼 추가 */}
+        <button className={styles.closeButton} onClick={onClose}>
+          닫기
+        </button>
+      </div>
+      <div className={styles.triangle}></div> {/* 삼각형 모양 추가 */}
     </div>
   );
 };
