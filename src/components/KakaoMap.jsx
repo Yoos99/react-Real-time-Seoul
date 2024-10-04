138f1a5de643ffd9227937de17ea2d1efd496767
Yoos99 <yoosh2520@naver.com>
2024-10-04 23:57:51 +0900

    feat: 맵 선택시 카드 초기화

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -3,14 +3,15 @@ import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import AreaCard from './AreaCard';
 
 function KakaoMap({ areaData }) {
-  const [selectedArea, setSelectedArea] = useState(null);
+  const [selectedArea, setSelectedArea] = useState(null); // 선택된 지역 정보 저장
 
   return (
-    <div>
+    <div style={{ position: 'relative' }}>
       <Map
         center={{ lat: 37.55659428234287, lng: 126.97302795181167 }}
         style={{ width: '1500px', height: '1000px' }}
         level={7}
+        onClick={() => setSelectedArea(null)} // 맵을 클릭하면 카드 닫기
       >
         {/* 모든 지역 데이터를 이용해 마커를 생성 */}
         {areaData &&
@@ -20,14 +21,17 @@ function KakaoMap({ areaData }) {
               key={index}
               position={{ lat: parseFloat(area.x), lng: parseFloat(area.y) }}
               clickable={true}
-              onClick={() => setSelectedArea(area)}
+              onClick={() => setSelectedArea(area)} // 마커 클릭 시 지역 정보 설정
             />
           ))}
       </Map>
 
-      {/* 선택된 지역의 상세 정보를 출력 */}
+      {/* 선택된 지역의 상세 정보를 출력 (카드) */}
       {selectedArea && (
-        <AreaCard area={selectedArea} onClose={() => setSelectedArea(null)} />
+        <AreaCard
+          area={selectedArea}
+          onClose={() => setSelectedArea(null)} // 카드 닫기 버튼 설정
+        />
       )}
     </div>
   );
