f06a5dae8a943c221381f602f19e59dd17df49b6
Yoos99 <yoosh2520@naver.com>
2024-10-07 11:35:44 +0900

    feat: 도넛 차트 생성

---
+++ src/components/DonutChart.jsx
@@ -0,0 +1,70 @@
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+// Chart.js에서 사용할 요소 등록
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function DonutChart({ data }) {
+  // 도넛 차트를 위한 데이터 설정
+  const chartData = {
+    labels: ['10대', '20대', '30대', '40대', '50대', '60대'],
+    datasets: [
+      {
+        label: '연령대별 비율',
+        data: [
+          data.PPLTN_RATE_10,
+          data.PPLTN_RATE_20,
+          data.PPLTN_RATE_30,
+          data.PPLTN_RATE_40,
+          data.PPLTN_RATE_50,
+          data.PPLTN_RATE_60,
+        ],
+        backgroundColor: [
+          '#FF6384', // 10대
+          '#FF9F40', // 20대
+          '#FFCD56', // 30대
+          '#4BC0C0', // 40대
+          '#36A2EB', // 50대
+          '#9966FF', // 60대
+        ],
+        hoverOffset: 4,
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  // 도넛 차트 옵션 설정
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    cutout: '65%', // 도넛 차트의 중앙 비율 설정
+    plugins: {
+      legend: {
+        display: true,
+        position: 'right',
+        labels: {
+          padding: 30, // 범례의 각 항목과 간격 설정
+          font: {
+            size: 20, // 범례의 폰트 크기 설정
+          },
+        },
+      },
+      tooltip: {
+        enabled: true,
+      },
+    },
+    layout: {
+      padding: {
+        right: 10, // 도넛 차트와 범례 사이의 간격 조절
+      },
+    },
+  };
+
+  return (
+    <div style={{ height: '460px', width: '460px', margin: '0 auto' }}>
+      <Doughnut data={chartData} options={chartOptions} />
+    </div>
+  );
+}
+
+export default DonutChart;
---
+++ src/components/PlaceCard.jsx
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+import { findHighestAgeGroup } from '../utils/populationCalculate'; // 필요한 유틸리티 함수 가져오기
+import styles from './PlaceCard.module.scss'; // 스타일 가져오기
+import DonutChart from './DonutChart'; // DonutChart 컴포넌트 가져오기
+
+function PlaceCard({ hotPlace }) {
+  const [isExpanded, setIsExpanded] = useState(false); // 카드 확장 여부 상태 관리
+
+  // 가장 인기가 많은 나이대 계산
+  const highestAgeGroup = findHighestAgeGroup(hotPlace);
+  const congestLvl = hotPlace.AREA_CONGEST_LVL;
+
+  // 혼잡도에 따른 클래스 설정
+  const congestionLevelClass = (congestLvl) => {
+    switch (congestLvl) {
+      case '붐빔':
+        return styles['very-crowded'];
+      case '약간 붐빔':
+        return styles['crowded'];
+      case '보통':
+        return styles['normal'];
+      case '여유':
+        return styles['relaxed'];
+      default:
+        return '';
+    }
+  };
+
+  // 연령대별 클래스 설정
+  const ageGroupClass = (ageGroup) => {
+    switch (ageGroup) {
+      case '10대':
+        return styles['age-10'];
+      case '20대':
+        return styles['age-20'];
+      case '30대':
+        return styles['age-30'];
+      case '40대':
+        return styles['age-40'];
+      case '50대':
+        return styles['age-50'];
+      case '60대':
+        return styles['age-60'];
+      default:
+        return '';
+    }
+  };
+
+  // 가장 한적한 시간 구하기
+  const leastCongestedTime = hotPlace.FCST_PPLTN.reduce((minTime, current) =>
+    parseFloat(current.FCST_PPLTN_MIN) < parseFloat(minTime.FCST_PPLTN_MIN)
+      ? current
+      : minTime
+  ).FCST_TIME;
+
+  return (
+    <div
+      className={`${styles.card} ${isExpanded ? styles.expanded : ''}`}
+      onClick={() => setIsExpanded(!isExpanded)}
+    >
+      <div className={styles.cardImage}>
+        <img
+          src={`https://data.seoul.go.kr/SeoulRtd/images/hotspot/${hotPlace.AREA_NM}.jpg`}
+          alt={hotPlace.AREA_NM}
+          className={styles.image}
+        />
+      </div>
+      <div className={styles.cardContent}>
+        <h2>{hotPlace.AREA_NM}</h2>
+        <p>{hotPlace.AREA_CONGEST_MSG}</p> {/* 지역의 혼잡 메시지 */}
+        <div className={styles.ppltnInfo}>
+          <div className={styles.metrics}>
+            <div className={styles.metric}>
+              <img src="/src/img/man.svg" alt="Male" />
+              {hotPlace.MALE_PPLTN_RATE}%
+            </div>
+            <div className={styles.metric}>
+              <img src="/src/img/woman.svg" alt="Female" />
+              {hotPlace.FEMALE_PPLTN_RATE}%
+            </div>
+          </div>
+          <div className={styles.tags}>
+            <span
+              className={`${styles.tag} ${congestionLevelClass(
+                hotPlace.AREA_CONGEST_LVL
+              )}`}
+            >
+              {congestLvl}
+            </span>
+            <span
+              className={`${styles.ageTag} ${ageGroupClass(highestAgeGroup)}`}
+            >
+              {highestAgeGroup}에게 인기 많아요!
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {/* 카드를 클릭하면 확장된 상태에서 차트를 보여줌 */}
+      {isExpanded && (
+        <div className={styles.chartContainer}>
+          <h3 className={styles.chartTitle}>
+            {leastCongestedTime.slice(-5)}에 가장 한적해요!
+          </h3>
+          <DonutChart data={hotPlace} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default PlaceCard;
